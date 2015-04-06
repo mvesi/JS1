94318e53498b5e2f8c96bed763d35fee3d1cc9ec
Michael Genovesi <michaelgenovesi@gmail.com>
2015-04-06 10:45:33 -0600

    completed exercise

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
+// Setup
+// Create a blank, valid HTML5 document
+// Create a javascript file with the content alert('Hello Boulder!'); and include it in your page with the script tag
+// Check Your Work: When you refresh the page, you should see an alert box that interrupts you with the message, "Hello Boulder!"
+// Run git add -A and git commit -m "[ENTER MESSAGE]" to commit your changes to your local repo.
+// Create a repo on github using the instructions in How to Git. Push your changes to the remote github repo.
+
 alert('Hello Boulder!');
+
+
+// Part II: Personal Hello
+// Create a blank, valid HTML5 document
+// Create a javascript file and include it with a script tag as before
+// Use var, prompt, string literals, the + operator, and alert such that when the user opens the page, it will ask them for their name, and then respond with "Hello [YOUR_NAME]!"
+
+
+var name = prompt("What is your name?");
+
+alert('Hello, ' + name);
